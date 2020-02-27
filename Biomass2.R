0a39c9c1bd31b4ff8ffdddafc4dce16a16949408
Laura Ploughe <ploughel@gmail.com>
2020-02-27 15:32:01 -0800

    Create Biomass2.R

---
+++ Biomass2.R
@@ -0,0 +1,262 @@
+#Biomass meta-analysis
+library(metafor)
+library(tidyverse)
+library(ggplot2)
+
+library(car)
+library(vegan)
+
+library(meta)
+library(effsize)
+library(esc)
+library(metaviz)
+
+
+library(readxl)
+Biomass <- read_excel("~/Documents/GitHub/Grassland restoration with biosolids/data_sheets.xlsx", 
+                    sheet = "Biomass")
+
+
+ROM.Biomass <- escalc(n2i = Nc, n1i = Ne, m2i = Mc, m1i = Me, 
+                    sd2i = scimp, sd1i = seimp, data = Biomass, measure = "ROM", 
+                    append = TRUE )
+
+
+HG.Biomass <- escalc(n2i = Nc, n1i = Ne, m2i = Mc, m1i = Me, 
+                   sd2i = scimp, sd1i = seimp, data = Biomass, measure = "SMD", 
+                   append = TRUE )
+
+
+Hedges = HG.Biomass$yi
+# #
+h <- hist(Hedges, breaks = 10, density = 10,
+          xlab = "Hedges' (g)")
+xfit <- seq(min(Hedges,na.rm=TRUE), max(Hedges,na.rm=TRUE), length = 40)
+yfit <- dnorm(xfit, mean = mean(Hedges,na.rm=TRUE), sd = sd(Hedges,na.rm=TRUE))
+yfit <- yfit * diff(h$mids[1:2]) * length(Hedges)
+# #
+lines(xfit, yfit, col = "black", lwd = 2)
+# #
+LRR = ROM.Biomass$yi
+# #
+r <- hist(LRR, breaks = 10, density = 10, xlab = "Log Response Ratio")
+xfit.r <- seq(min(LRR), max(LRR), length = 40)
+yfit.r <- dnorm(xfit.r, mean = mean(LRR), sd = sd(LRR))
+yfit.r <- yfit.r * diff(r$mids[1:2]) * length(LRR)
+# #
+lines(xfit.r, yfit.r, col = "black", lwd = 2)
+#
+
+
+#random effects model
+
+rand.var=list(~ 1|Paper.ID./Case)
+
+rma.random <- rma.mv(yi = yi, V = vi, random = rand.var, data = ROM.Biomass, method = "ML")
+summary(rma.random)
+ran.df<-as.data.frame(coef(summary(rma.random)))
+
+
+#-----------------------# model selection 
+library(xlsx)
+library(rJava)
+library("glmulti")
+
+
+ROM.Biomass<-ROM.Biomass %>% 
+  filter(yi!="NA")
+
+ROM.ai.size <- glmulti(yi ~Biosolid.level..Mg.ha.1.+time+Temp+Precip+Mixture..yes.no.+Burn..Y.N.+
+                        Seeded..Y.N.+Multiple.application..Y.N.+Severe.Disturbance+ai, data=ROM.Biomass, method="d",
+                      level=1, crit="aicc")
+
+ROM.ai.mod <- glmulti(yi ~ Biosolid.level..Mg.ha.1.+time+Temp+Precip+Mixture..yes.no.+Burn..Y.N.+
+                       Seeded..Y.N.+Multiple.application..Y.N.+Severe.Disturbance+ai, data=ROM.Biomass, method="h",
+                     level=1, crit="aicc", confsetsize=ROM.ai.size)
+
+plot(ROM.ai.mod, type="s")
+
+
+
+
+ROM.ai.rma<-rma.mv(yi, vi, mods = ~time+Temp+Precip+Mixture..yes.no.+Burn..Y.N.+Seeded..Y.N.+Severe.Disturbance+ai, 
+                       random = rand.var,data = ROM.Biomass,slab = paste(author, year), method="ML")
+vif(ROM.ai.rma, table=TRUE) 
+summary(ROM.ai.rma)
+
+
+
+mods = list(ROM.ai.rma)
+
+
+
+# model fit diagnostics: pseudo R2, marginal/conditional R2, AICc, and I2
+pseudo.r2 <- c()
+marg.r2 <- c()
+cond.r2 <- c()
+aicc <- c()
+I2 <- c()
+
+for (i in 1:length(mods)) {
+  # pseudo R2 of proportional reduction of variance explained when fitting reduced model relative to model with random effects only
+  pseudo.r2[i] <- (sum(rma.random$sigma2) - sum(mods[[i]]$sigma2)) / sum(rma.random$sigma2)
+  
+  # marginal and conditional R2 for linear mixed-mods from Nakagawa and Scheilzeth 2013
+  # variance from fixed effects
+  fe.total <- c()
+  for (j in 2:ncol(model.matrix(mods[[i]]))) {
+    fe <- mods[[i]]$b[j] * model.matrix(mods[[i]])[, j] 
+    fe.total <- c(fe.total, fe)
+  }
+  v.fix <- var(fe.total)
+  # variance from random effects
+  v.rand <- sum(mods[[i]]$sigma2)
+  # variance from residuals
+  v.resid <- var(residuals(mods[[i]]))
+  # marginal R2 - total variance explained from fixed effects
+  marg.r2[i] <- v.fix / (v.fix + v.rand + v.resid)
+  # conditional R2 - total variance explained from fixed and random effects
+  cond.r2[i] <- (v.fix + v.rand) / (v.fix + v.rand + v.resid)
+  
+  # from Wolfgang Viechtbauer (http://www.metafor-project.org/doku.php/tips:i2_multilevel_multivariate)
+  # I2 statistic - amount of heterogeneity relative to the total amount of variance in observed effects (how much heterogeneity contributes to total variance)
+  W <- diag(1/ROM.Biomass$vi)
+  X <- model.matrix(mods[[i]])
+  P <- W - W %*% X %*% solve(t(X) %*% W %*% X) %*% t(X) %*% W
+  I2[i] <- 100 * sum(mods[[i]]$sigma2) / (sum(mods[[i]]$sigma2) + (mods[[i]]$k - mods[[i]]$p) / sum(diag(P)))
+  
+  aicc[i] <- fitstats(mods[[i]])[5]
+}
+# 
+mods.int.df<-cbind(pseudo.r2,marg.r2,cond.r2,aicc,I2)
+
+
+#=========================================graphs=========
+
+
+
+write.csv(as.data.frame(coef(summary(ROM.ai.rma))), file="prod.mod.csv")
+df.prod<-read.csv("prod.mod.csv")
+df.prod
+
+df.ai<-df.prod[2:9,]
+
+LRR.ai.prod = ggplot(data=df.ai,
+                        aes(x = X, y = estimate, ymin =ci.lb , ymax = ci.ub ))+
+  geom_point(aes(color=X))+
+  geom_hline(yintercept =0, linetype=2)+
+  scale_color_brewer(type = 'div', palette = 4)+
+  xlab('ai')+ ylab("Log Response Ratio")+
+  geom_errorbar(aes(ymin=ci.lb, ymax=ci.ub,col=X),width=0.3,cex=1)+ 
+  #xlim(breaks =rev(mod.lev))+
+  #facet_wrap(~X,scale="free") +
+  theme(plot.title=element_text(size=16,face="bold"),
+        axis.text.x=element_text(face="bold"),
+        axis.title=element_text(size=12,face="bold"),
+        legend.position = c(.8,.7), axis.text.y=element_blank())+
+  theme(axis.ticks.length=unit(.5, "cm"), axis.text=element_text(size=20),
+        axis.title=element_text(size=20),legend.title=element_text(size=15), 
+        legend.text=element_text(size=13))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))  +
+  
+  
+  coord_flip()
+LRR.ai.prod
+
+# tiff("prod.ai.tiff", width = 20, height= 12, units ='cm', res=600)
+# LRR.ai.prod
+# dev.off()
+
+
+
+preds<-predict(ROM.ai.rma, addx = TRUE)
+
+names(preds)
+
+preds<-as.data.frame(preds)
+names(preds)
+
+
+
+# write.csv(preds, file = "lrr.prod.csv")
+lrr.prod<-read.csv("lrr.prod.csv", header = TRUE)
+head(lrr.prod)
+
+#forest plot
+
+forest.prod<-viz_forest(x =preds[c("pred", "se")], variant="classic",summary_label ="Summary Effect", xlab = "Log Response Ratio")
+tiff("prod.forest.tiff", width = 16, height= 25, units ='cm', res=600)
+forest.prod
+dev.off()
+
+ 
+
+#ai
+head(preds)
+bubble.ai=ggplot(preds, aes(x=X.ai,y=pred))+
+  stat_smooth(data=preds,method="glm",formula=y~x,fullrange=T,se=TRUE,size=1, color="black"     )+ #, linetype=Biome 
+  geom_hline(yintercept=0, linetype="dashed", size=.5)+
+  # stat_smooth(data=preds.biome,aes(x=level.exp,y=ci.lb, color=Biome),method="glm",formula=y~x,fullrange=T,
+  #             se=FALSE,size=1,linetype=3)+
+  # stat_smooth(data=preds.biome,aes(x=level.exp,y=ci.ub),method="glm",formula=y~x,fullrange=T,
+  #             se=FALSE,size=1,linetype=3)+
+  # scale_linetype_manual(values=biome.line)+
+  geom_point(data=ROM.Biomass, aes(x=ai, y = yi, size=1/vi))+
+  ylab("Log Response Ratio")+
+  xlab("Aridity Index")+
+  theme(axis.ticks.length=unit(.5, "cm"), axis.text=element_text(size=20),
+        axis.title=element_text(size=20), legend.position = "bottom")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+bubble.ai
+
+# tiff("prod.ai.tiff", width = 16, height= 12, units ='cm', res=600)
+# bubble.ai
+# dev.off()
+
+
+bubble.p=ggplot(preds, aes(x=X.Precip,y=pred))+
+  stat_smooth(data=preds,method="glm",formula=y~x,fullrange=T,se=TRUE,size=1, color="black"     )+ #, linetype=Biome 
+  geom_hline(yintercept=0, linetype="dashed", size=.5)+
+  # stat_smooth(data=preds.biome,aes(x=level.exp,y=ci.lb, color=Biome),method="glm",formula=y~x,fullrange=T,
+  #             se=FALSE,size=1,linetype=3)+
+  # stat_smooth(data=preds.biome,aes(x=level.exp,y=ci.ub),method="glm",formula=y~x,fullrange=T,
+  #             se=FALSE,size=1,linetype=3)+
+  # scale_linetype_manual(values=biome.line)+
+  geom_point(data=ROM.Biomass, aes(x=Precip, y = yi, size=1/vi))+
+  ylab("Log Response Ratio")+
+  xlab("Precipitation (cm)")+
+  theme(axis.ticks.length=unit(.5, "cm"), axis.text=element_text(size=20),
+        axis.title=element_text(size=20), legend.position = "bottom")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+bubble.p
+
+#  tiff("prod.p.tiff", width = 16, height= 12, units ='cm', res=600)
+# bubble.p
+#  dev.off()
+
+bubble.t=ggplot(preds, aes(x=X.time,y=pred))+
+  stat_smooth(data=preds,method="glm",formula=y~x,fullrange=T,se=TRUE,size=1, color="black"     )+ #, linetype=Biome 
+  geom_hline(yintercept=0, linetype="dashed", size=.5)+
+  # stat_smooth(data=preds.biome,aes(x=level.exp,y=ci.lb, color=Biome),method="glm",formula=y~x,fullrange=T,
+  #             se=FALSE,size=1,linetype=3)+
+  # stat_smooth(data=preds.biome,aes(x=level.exp,y=ci.ub),method="glm",formula=y~x,fullrange=T,
+  #             se=FALSE,size=1,linetype=3)+
+  # scale_linetype_manual(values=biome.line)+
+  geom_point(data=ROM.Biomass, aes(x=time, y = yi, size=1/vi))+
+  ylab("Log Response Ratio")+
+  xlab("Years Since Restoration")+
+  theme(axis.ticks.length=unit(.5, "cm"), axis.text=element_text(size=20),
+        axis.title=element_text(size=20), legend.position = "bottom")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+bubble.t
+
+# tiff("prod.t.tiff", width = 16, height= 12, units ='cm', res=600)
+# bubble.t
+# dev.off()
